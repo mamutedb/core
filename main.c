d35322c44c699f96096af030150b2fabade7cd11
ahsouza <annibalhsouza@gmail.com>
2023-01-05 17:38:24 -0300

    implement method to perform simple insertion and select of data in memory array

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 // if compiling on Windows compile these functions
 #ifdef _WIN32
 #include <string.h>
+#include <stdint.h>
+ 
 
 static char buffer[2048];
 
@@ -15,7 +17,76 @@ char* readLine(char* prompt) {
   return cpy;
 }
 
-void getLine(char* unused) {}
+
+typedef enum { EXECUTE_SUCCESS, EXECUTE_TABLE_FULL } ExecuteResult;
+typedef enum {
+  META_COMMAND_SUCCESS,
+  META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+typedef enum { PREPARE_SUCCESS, PREPARE_SYNTAX_ERROR, PREPARE_UNRECOGNIZED_STATEMENT } PrepareResult;
+typedef enum { STATEMENT_INSERT, STATEMENT_SELECT } StatementType;
+
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+typedef struct {
+  uint32_t id;
+  char username[COLUMN_USERNAME_SIZE];
+  char email[COLUMN_EMAIL_SIZE];
+} Row;
+typedef struct {
+  StatementType type;
+  Row row_to_insert;
+} Statement;
+
+
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
+const uint32_t ID_SIZE = size_of_attribute(Row, id);
+const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
+const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+const uint32_t PAGE_SIZE = 4096;
+#define TABLE_MAX_PAGES 100
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
+const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+
+typedef struct {
+  uint32_t num_rows;
+  void* pages[TABLE_MAX_PAGES];
+} Table;
+
+void print_row(Row* row) {
+  printf("(%d, %s, %s)\n", row->id, row->username, row->email);
+}
+
+void serialize_row(Row* source, void* destination) {
+  memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
+  memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
+  memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+}
+
+void deserialize_row(void* source, Row* destination) {
+  memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
+  memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
+  memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
+}
+
+void* row_slot(Table* table, uint32_t row_num) {
+  uint32_t page_num = row_num / ROWS_PER_PAGE;
+  void* page = table->pages[page_num];
+  if (page == NULL) {
+    // Allocate memory only when we try to access page
+    page = table->pages[page_num] = malloc(PAGE_SIZE);
+  }
+  uint32_t row_offset = row_num % ROWS_PER_PAGE;
+  uint32_t byte_offset = row_offset * ROW_SIZE;
+  return page + byte_offset;
+}
+
+void writeHistory(char* unused) {}
 
 // otherwise include the editline headers
 #else
@@ -29,21 +100,146 @@ void close_input_buffer(char* prompt) {
 }
 // end free allocated memory
 
+
+
+MetaCommandResult do_meta_command(char* input) {
+  if (strcmp(input, ":q") == 0) {
+    close_input_buffer(input);
+    exit(EXIT_SUCCESS);
+  } else {
+    return META_COMMAND_UNRECOGNIZED_COMMAND;
+  }
+}
+
+PrepareResult prepare_statement(char* input, Statement* statement) {
+  
+  
+  
+  if (strncmp(input, "insert", 6) == 0) {
+    statement->type = STATEMENT_INSERT;
+
+    int args_assigned = sscanf(
+      input, "insert %d %s %s", &(statement->row_to_insert.id),
+      statement->row_to_insert.username, statement->row_to_insert.email
+    );
+    if (args_assigned < 3) {
+      return PREPARE_SYNTAX_ERROR;
+    }
+
+    return PREPARE_SUCCESS;
+  }
+
+  if (strcmp(input, "select") == 0) {
+    statement->type = STATEMENT_SELECT;
+    return PREPARE_SUCCESS;
+  }
+
+  return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+
+ExecuteResult execute_insert(Statement* statement, Table* table) {
+  if (table->num_rows >= TABLE_MAX_ROWS) {
+    return EXECUTE_TABLE_FULL;
+  }
+  Row* row_to_insert = &(statement->row_to_insert);
+  serialize_row(row_to_insert, row_slot(table, table->num_rows));
+  table->num_rows += 1;
+
+  return EXECUTE_SUCCESS;
+}
+
+ExecuteResult execute_select(Statement* statement, Table* table) {
+  Row row;
+  for (uint32_t i = 0; i < table->num_rows; i++) {
+    deserialize_row(row_slot(table, i), &row);
+    print_row(&row);
+  }
+  return EXECUTE_SUCCESS;
+}
+
+ExecuteResult execute_statement(Statement* statement, Table* table) {
+  switch (statement->type) {
+    case (STATEMENT_INSERT):
+      // printf("This is where we would do an insert.\n");
+      // break;
+      return execute_insert(statement, table);
+    case (STATEMENT_SELECT):
+      // printf("This is where we would do a select.\n");
+      // break;
+      return execute_select(statement, table);
+  }
+  return EXECUTE_SUCCESS;
+}
+
+Table* new_table() {
+  Table* table = (Table*)malloc(sizeof(Table));
+  table->num_rows = 0;
+  for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++) {
+     table->pages[i] = NULL;
+  }
+  return table;
+}
+
+void free_table(Table* table) {
+  for (int i = 0; table->pages[i]; i++) {
+    free(table->pages[i]);
+  }
+  free(table);
+}
+
+
 int main(int argc, char** argv) {
+  Table* table = new_table();
 
   puts("Mamute DB v0.0.1");
-  puts("\nType ':exit' To Exit\n");
+  puts("\nType ':q' To Exit\n");
 
   while (1) {
-    char* input = readLine("mamute> ");
-    getLine(input);
 
-    if (strcmp(input, ":exit") == 0) {
+    char* input = readLine("mamute> ");
+    writeHistory(input);
+    // printf(": '%s'.\n", input);
+    if (strcmp(input, ":q") == 0) {
       close_input_buffer(input);
       exit(EXIT_SUCCESS);
-    } else {
-      printf("comando desconhecido: '%s'.\n", input);
     }
+    //  else {
+      // printf("%c",input[0]);
+    // }
+
+    if (input[0] == ':') {
+      switch (do_meta_command(input)) {
+        case (META_COMMAND_SUCCESS):
+          continue;
+        case (META_COMMAND_UNRECOGNIZED_COMMAND):
+          printf("Unrecognized command '%s'\n", input);
+          continue;
+      }
+    }
+
+    Statement statement;
+    switch (prepare_statement(input, &statement)) {
+      case (PREPARE_SUCCESS):
+        break;
+      case (PREPARE_SYNTAX_ERROR):
+        printf("Syntax error. Não foi possível analisar a instrução.\n");
+        continue;
+      case (PREPARE_UNRECOGNIZED_STATEMENT):
+        printf("Comando não reconhecido '%s'.\n", input);
+        continue;
+    }
+
+    // execute_statement(&statement, table);
+    switch (execute_statement(&statement, table)) {
+      case (EXECUTE_SUCCESS):
+        printf("\nexecutado com sucesso!\n");
+        break;
+      case (EXECUTE_TABLE_FULL):
+        printf("Error de armazenamento: Você atingiu o limite máximo de armazenamento.\n");
+        break;
+    }
+
   }
   return 0;
 }
